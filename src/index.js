dc570b2a1712a9fda1b90314c12296a06aa6a7b4
fernando39357 <fernando39357@gmail.com>
2020-08-12 11:32:52 -0300

    Eslint init and editorconfig generate

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-	response.send('Hello World');
+  response.send('Hello World');
 });
 
 app.listen(3000, () => {
-	console.log('Server started at http://localhost:3000');
+  console.log('Server started at http://localhost:3000');
 });
