cc27fb811be4db35aa1a89eb972b5434a219a235
fernando39357 <fernando39357@gmail.com>
2020-08-15 15:58:37 -0300

    Put contacts

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -30,7 +30,7 @@ class ContactController {
     const contactExists = await ContactsRepository.findByEmail(email);
 
     if (contactExists) {
-      return response.status(400).json({ error: 'This e-mail is already benn taken' });
+      return response.status(400).json({ error: 'This e-mail is already in use' });
     }
 
     const contact = await ContactsRepository.create({
@@ -40,8 +40,29 @@ class ContactController {
     response.json(contact);
   }
 
-  update() {
-    // Editar um registro
+  async update(request, response) {
+    const { id } = request.params;
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    const contactExists = await ContactsRepository.findById(id);
+    if (!contactExists) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+    const contactByEmail = await ContactsRepository.findByEmail(email);
+
+    if (contactByEmail && contactByEmail.id !== id) {
+      return response.status(400).json({ error: 'This e-mail is already in use' });
+    }
+
+    const contact = await ContactsRepository.update(id, {
+      name, email, phone, category_id,
+    });
+    response.json(contact);
   }
 
   async delete(request, response) {
